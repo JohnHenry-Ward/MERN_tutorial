e7a4a6cbcd5b62c9336dc6ec72a4f61650b75236
JohnHenry <johnhenry514@gmail.com>
2022-01-14 13:55:19 -0800

    added react/redux state to handle errors and authorization"

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 //here we want to combine all of our reducers to this one file to make things cleaner
 import { combineReducers } from "redux";
 import itemReducer from './itemReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    item: itemReducer
+    item: itemReducer,
+    error: errorReducer,
+    auth: authReducer
 })
